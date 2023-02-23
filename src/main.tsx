c8feda09d11bbc70cb21ed3b44dd7436e91a71a7
Douglas <hemy426@gmail.com>
2023-02-23 15:50:52 -0300

    ajustes

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import '/src/styles/App.css'
 
-import '/src/components/styles/App.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
